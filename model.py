6949206c0578d43624ec287e8ab8d652dbae9fb9
Hubert Mądrzejewski <88489221+hubertmadrzejewski@users.noreply.github.com>
2023-06-20 01:12:38 +0200

    Pozbycie sie bledu ze zmienna lr

--- model.py
+++ model.py
@@ -23,11 +23,11 @@ class Linear_QNet(nn.Module):
         file_name = os.path.join(model_folder_path, file_name)
         torch.save(self.state_dict(), file_name)
 class QTrainer:
-    def __init__(self, model, learning_rate, gamma):
-        self.learning_rate = learning_rate
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
         self.gamma = gamma
         self.model = model
-        self.optimizer = optim.Adam(model.parameters(), learning_rate=self.learning_rate)
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
         self.criterion = nn.MSELoss()
 
     def train_step(self, state, action, reward, next_state, done):
